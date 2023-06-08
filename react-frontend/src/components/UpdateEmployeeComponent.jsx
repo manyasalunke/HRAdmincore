1645f8761b13d5684e8b644dcc4c981d702b6e7e
Amol Chilka <manyasalunke86@gmail.com>
2023-06-08 15:42:18 +0530

    ter

---
+++ react-frontend/src/components/UpdateEmployeeComponent.jsx
@@ -0,0 +1,125 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService';
+
+class UpdateEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: this.props.match.params.id,
+            firstName: '',
+            lastName: '',
+            emailId: '',
+            phonenumber: '',
+            address: '',
+            dob: ''
+        }
+        this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
+        this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+        this.updateEmployee = this.updateEmployee.bind(this);
+    }
+
+    componentDidMount(){
+        EmployeeService.getEmployeeById(this.state.id).then( (res) =>{
+            let employee = res.data;
+            this.setState({firstName: employee.firstName,
+                lastName: employee.lastName,
+                emailId : employee.emailId,
+                phonenumber : employee.phonenumber,
+                address : employee.address,
+                dob : employee.dob
+            });
+        });
+    }
+
+    updateEmployee = (e) => {
+        e.preventDefault();
+        let employee = {firstName: this.state.firstName, lastName: this.state.lastName, emailId: this.state.emailId, phonenumber: this.state.phonenumber, address: this.state.address, dob: this.state.dob};
+        console.log('employee => ' + JSON.stringify(employee));
+        console.log('id => ' + JSON.stringify(this.state.id));
+        EmployeeService.updateEmployee(employee, this.state.id).then( res => {
+            this.props.history.push('/employees');
+        });
+    }
+    
+    changeFirstNameHandler= (event) => {
+        this.setState({firstName: event.target.value});
+    }
+
+    changeLastNameHandler= (event) => {
+        this.setState({lastName: event.target.value});
+    }
+
+    changeEmailHandler= (event) => {
+        this.setState({emailId: event.target.value});
+    }
+    changePhonenumberHandler= (event) => {
+        this.setState({phonenumber: event.target.value});
+    }
+    changeAddressHandler= (event) => {
+        this.setState({address: event.target.value});
+    }
+
+    changeDobHandler= (event) => {
+        this.setState({dob: event.target.value});
+    }
+
+    cancel(){
+        this.props.history.push('/employees');
+    }
+
+    render() {
+        return (
+            <div>
+                <br></br>
+                   <div className = "container">
+                        <div className = "row">
+                            <div className = "card col-md-6 offset-md-3 offset-md-3">
+                                <h3 className="text-center">Update Employee</h3>
+                                <div className = "card-body">
+                                    <form>
+                                        <div className = "form-group">
+                                            <label> First Name: </label>
+                                            <input placeholder="First Name" name="firstName" className="form-control" 
+                                                value={this.state.firstName} onChange={this.changeFirstNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Last Name: </label>
+                                            <input placeholder="Last Name" name="lastName" className="form-control" 
+                                                value={this.state.lastName} onChange={this.changeLastNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Email Id: </label>
+                                            <input placeholder="Email Address" name="emailId" className="form-control" 
+                                                value={this.state.emailId} onChange={this.changeEmailHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Phone Number: </label>
+                                            <input placeholder="Phone Number" name="phonenumber" className="form-control" 
+                                                value={this.state.phonenumber} onChange={this.changePhonenumberHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Address: </label>
+                                            <input placeholder="Address" name="address" className="form-control" 
+                                                value={this.state.address} onChange={this.changeAddressHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Dob: </label>
+                                            <input placeholder="Dob" name="dob" className="form-control" 
+                                                value={this.state.dob} onChange={this.changeDobHandler}/>
+                                        </div>
+
+                                        <button className="btn btn-success" onClick={this.updateEmployee}>Save</button>
+                                        <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                   </div>
+            </div>
+        )
+    }
+}
+
+export default UpdateEmployeeComponent
